56338d28fa80908c5fb411855fe67f28f64f7e7a
marco-knietzsch <71555548+marco-knietzsch@users.noreply.github.com>
2024-11-28 10:16:25 +0100

    init

---
+++ Timberborn_FreeCam/ModStarter.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using Timberborn.ModManagerScene;
+
+namespace Timberborn_FreeCam
+{
+    public class ModStarter : IModStarter
+    {
+        public void StartMod(IModEnvironment modEnvironment)
+        {
+            Harmony harmony = new("com.marcok.freecam");
+            Harmony.DEBUG = true;
+            harmony.PatchAll();
+        }
+    }
+}
---
+++ Timberborn_FreeCam/Patches.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using Timberborn.CameraSystem;
+using Timberborn.Common;
+
+namespace Timberborn_FreeCam
+{
+    [HarmonyPatch]
+    public class Patches
+    {
+        [HarmonyPatch(typeof(CameraComponent), "ZoomLimits", MethodType.Getter)]
+        public static bool Prefix(CameraComponent __instance, ref FloatLimits __result)
+        {
+            AccessTools.FieldRef<CameraComponent, FloatLimits> freeModeZoomLimits = AccessTools.FieldRefAccess<CameraComponent, FloatLimits>("_freeModeZoomLimits");
+            __result = freeModeZoomLimits(__instance);
+            return false;
+        }
+    }
+}
